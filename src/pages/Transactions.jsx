9cc617853961e9524ba6bcd5382b188cddffda6c
fayozbekdew <fayozbekdev@gmail.com>
2024-12-16 23:39:29 +0500

    finished

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -2,24 +2,30 @@ import React, { useCallback, useState } from "react";
 import DatePickerCom from "../components/DatePickerCom";
 import FilterButton from "../components/FilterButton";
 import Modal from "../components/Modal";
-import TransactionsTable from "../components/TransactionsTable";
+import Table from "../components/Table";
 
 function Transactions() {
   const [size, setSize] = useState(null);
-  const [transactions, setTransactions] = useState(() => 
-  JSON.parse(localStorage.getItem('transactions')) || []);
+  const [transactions, setTransactions] = useState(
+    () => JSON.parse(localStorage.getItem("transactions")) || []
+  );
   const handleOpen = (value) => setSize(value);
-  
-    const handleDelete = useCallback((id) => {
-        const updatedTransactions = transactions.filter((el) => el.id !== id);
-        setTransactions(updatedTransactions);
-        localStorage.setItem('transactions', JSON.stringify(updatedTransactions));
-    }, [transactions]);
-    
-    console.log(1111111111111111111)
+  const handleDelete = useCallback(
+    (id) => {
+      const updatedTransactions = transactions.filter((el) => el.id !== id);
+      setTransactions(updatedTransactions);
+      localStorage.setItem("transactions", JSON.stringify(updatedTransactions));
+    },
+    [transactions]
+  );
+
   return (
     <div>
-      <Modal setTransactions={setTransactions} size={size} handleOpen={handleOpen} />
+      <Modal
+        setTransactions={setTransactions}
+        size={size}
+        handleOpen={handleOpen}
+      />
       <span className="flex gap-4 items-center justify-between">
         <button
           onClick={() => handleOpen("xs")}
@@ -40,11 +46,14 @@ function Transactions() {
           Add Transaction
         </button>
         <span className="flex items-center gap-x-4">
-        <DatePickerCom setTransactions={setTransactions} />
-        <FilterButton setTransactions={setTransactions} transactions={transactions} />
+          <DatePickerCom setTransactions={setTransactions} />
+          <FilterButton
+            setTransactions={setTransactions}
+            transactions={transactions}
+          />
         </span>
       </span>
-      <TransactionsTable onDelete={handleDelete} elements={transactions}/>
+      <Table onDelete={handleDelete} elements={transactions} />
     </div>
   );
 }
